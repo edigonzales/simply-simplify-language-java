07548c18300146fd7581718045299d0f38dcfbb7
Stefan Ziegler <edi.gonzales@gmail.com>
2024-06-13 11:43:13 +0200

    do statistical stuff

--- src/main/java/dev/edigonzales/service/AnalyzeService.java
+++ src/main/java/dev/edigonzales/service/AnalyzeService.java
@@ -1,9 +1,13 @@
 package dev.edigonzales.service;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.apache.avro.generic.GenericRecord;
 import org.apache.hadoop.conf.Configuration;
@@ -14,13 +18,27 @@ import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import jakarta.annotation.PostConstruct;
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.tokenize.SimpleTokenizer;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.parquet.Log;
 import org.apache.parquet.avro.AvroParquetReader;
 import org.apache.parquet.hadoop.ParquetReader;
 import org.apache.parquet.hadoop.util.HadoopInputFile;
 import org.apache.parquet.io.InputFile;
+import org.languagetool.AnalyzedSentence;
+import org.languagetool.AnalyzedTokenReadings;
+import org.languagetool.JLanguageTool;
+import org.languagetool.language.SwissGerman;
+
+import org.languagetool.tokenizers.Tokenizer;
+import org.languagetool.tokenizers.de.GermanWordTokenizer;
 
 
 @Service
@@ -30,7 +48,16 @@ public class AnalyzeService {
     @Value("classpath:/data/word_scores.parquet")
     private Resource wordScoresParquet;
 
-    private HashMap<String,Integer> wordScores = null;;
+    @Value("classpath:/data/opennlp-de-ud-gsd-sentence-1.0-1.9.3.bin")
+    private Resource openNlpSentenceDetectionModel;
+
+    @Value("classpath:/data/opennlp-de-ud-gsd-tokens-1.0-1.9.3.bin")
+    private Resource openNlpTokensModel;
+
+    @Value("classpath:/data/opennlp-de-ud-gsd-pos-1.0-1.9.3.bin")
+    private Resource openNlpPosModel;
+    
+    private HashMap<String,Integer> wordScores = null;
         
     public double getUnderstandability(String text) throws IOException {
         text = punctuateParagraphsAndBulletedLists(text);
@@ -43,9 +70,136 @@ public class AnalyzeService {
     /* Extract text features from text.
      */
     private void extractTextFeatures(String text) throws IOException {
-        System.err.println(this.wordScores);
+//        System.err.println(this.wordScores);
+        
+//        SentenceModel sentenceModel = new SentenceModel(openNlpSentenceDetectionModel.getFile());
+//        SentenceDetectorME sentenceDetector = new SentenceDetectorME(sentenceModel);
+//        
+//        String[] sentences = sentenceDetector.sentDetect(text);
+//        
+//        for (String sentence : sentences) {
+//            logger.info(sentence.toString());            
+//        }
+//        
+//        TokenizerModel model = new TokenizerModel(openNlpTokensModel.getFile());
+//        TokenizerME tokenizer = new TokenizerME(model);
+//        String[] tokens = tokenizer.tokenize(text);
+//        
+//        for (String token : tokens) {
+//            if (Pattern.matches("\\p{Punct}", token) || isNumeric(token)) {
+//                continue;
+//            }
+//            System.err.println(token);
+//        }
+
+        
+        JLanguageTool langTool = new JLanguageTool(new SwissGerman());
+
+        int docLength = 0;
+        int docScores = 0;
+        List<AnalyzedSentence> analyzedSentences = langTool.analyzeText(text);
+        for (AnalyzedSentence analyzedSentence : analyzedSentences) {
+            for (AnalyzedTokenReadings analyzedTokens : analyzedSentence.getTokensWithoutWhitespace()) {
+                if (analyzedTokens.getReadings().size() > 0) {
+                    String token = analyzedTokens.getReadings().get(0).getToken();
+                    if (!Pattern.matches("\\p{Punct}", token) && token.trim().length() > 0) {
+                        String lemma = analyzedTokens.getReadings().get(0).getLemma();
+                        if (lemma != null) {
+                            if (wordScores.containsKey(lemma.toLowerCase())) {
+                                docScores += wordScores.get(lemma.toLowerCase());
+                            }
+                        }
+                        docLength++;
+                    }
+                }
+            }
+        }
+        
+        double commonWordScore = docScores / docLength;
+        System.err.println(commonWordScore);
+        
+        
+        System.err.println("text: " + text);
+        for (AnalyzedSentence analyzedSentence : analyzedSentences) {
+            System.out.println(analyzedSentence.getText());
+        }
+        
+        
+//        Tokenizer tokenizer = new GermanWordTokenizer();
+//        List<String> tokens = tokenizer.tokenize(text);
+
+        // Print the tokens
+//        for (String token : tokens) {
+//            System.out.println(token);
+//        }
+
+        
+//        List<String> filteredTokens = tokens.stream()
+//                .filter(token -> !token.trim().isEmpty())
+//                .filter(token -> !isNumeric(token))
+//                .filter(token -> {
+//                    if (Pattern.matches("\\p{Punct}", token)) {
+//                        return false;
+//                    }
+//                    return true;
+//                })
+//                .collect(Collectors.toList());
+//
+//        
+//        for (String token : filteredTokens) {
+//            System.out.println(token);
+//        }
+
+        
+
+        
+//        JLanguageTool langTool = new JLanguageTool(Languages.getLanguageForShortCode("de"));
+//        langTool.tok
+
+        
+//        POSModel posModel = new POSModel(openNlpPosModel.getFile());
+//        POSTaggerME posTagger = new POSTaggerME(posModel);
+        
+//        Stream<String> tokensStream = Arrays.stream(tokens).filter(f->{
+//            System.err.println(f);
+//            return true;
+//        }); 
+//        logger.info(tokensStream.toString());
+//        
+//        tokensStream
+//            .filter(t -> {
+//                System.err.println(t);
+//                if (Pattern.matches("\\p{Punct}", t)) {
+//                    return false;
+//                }
+//                return true;
+//            })
+//            .filter(t -> {
+//                System.err.println(t);
+//                return true;
+//            });
+
+
+//        for (String token : tokens) {
+//            logger.info("{}", Pattern.matches("\\p{Punct}", token));
+//
+//            
+//            logger.info(token.toString());            
+//        }
+
+
+
+        
     }
     
+    public static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 
     /*
      * Get common word scores from the parquet file.
@@ -93,8 +247,10 @@ public class AnalyzeService {
      * the understandability score.
      */
     private String punctuateParagraphsAndBulletedLists(String text) {
+        // TODO Ist das wirklich nötig? Führt bei mir dazu, dass es einen leeren Satz gibt 
+        // und ein zusätzlicher Punkt.
         // Add a space to the end of the text to properly process the last sentence.
-        text = text + " ";
+        // text = text + " ";
 
         // This regex pattern matches lines that do not end with a dot and are not empty.
         String pattern = "^(?!.*\\.$).+";
